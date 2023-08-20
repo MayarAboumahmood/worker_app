72e8b3cec4740237b015b2a17543a2cbbb430ea4
Maysam-ite <maysamsamer24@gmail.com>
2023-08-20 16:16:26 +0300

    afetr merge

--- worker_app/lib/view/screens/orders/orders_service.dart
+++ worker_app/lib/view/screens/orders/orders_service.dart
@@ -6,7 +6,37 @@ import '../../../constant/status_request.dart';
 import '../../../data/checkInternet/check_internet.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../main.dart';
+
 class OrdersService {
+   Future<bool> checkingConfirmation() async {
+    int workerId = int.parse(await prefService.readString('id'));
+    var _client = http.Client();
+    var request = new http.Request(
+        "GET", Uri.parse('${ServerConstApis.baseAPI}sendEventID/${workerId}'));
+    request.headers["Cache-Control"] = "no-cache";
+    request.headers["Accept"] = "text/event-stream";
+
+    final response = await _client.send(request); // Await the response
+
+    if (response.statusCode == 200) {
+      await response.stream.listen((data) {
+        String d = utf8.decode(data);
+        d = d.trim();
+        print("Received data: $d");
+       print(d);
+        if (d != 'data: init') {
+          print("stored");
+          prefService.createString('enentI', "$d");
+        }
+        // Here, you can parse the data as needed and handle the event
+      }).asFuture(); // Await the stream subscription to complete
+    } else {
+      print("Received unexpected status code: ${response.statusCode}");
+    }
+    return true;
+  }
+
   Future<Either<StatuseRequest, Map>> denyOrder(String token,Map<String, String> data) async {
     //Either for return two data type in the same time
     try {
