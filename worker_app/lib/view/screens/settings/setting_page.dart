46e0ba5c35ef1a00d10de62245c04f8d472fe916
Mayar AB <mab026550@gmail.com>
2023-08-20 16:00:58 +0300

    applaying the logout in worker app.

--- worker_app/lib/view/screens/settings/setting_page.dart
+++ worker_app/lib/view/screens/settings/setting_page.dart
@@ -107,7 +107,8 @@ class Setting extends StatelessWidget {
   Widget logOut(Sizes size, BuildContext context) {
     return MaterialButton(
       onPressed: () {
-        //log out from the account.
+        Get.offAllNamed('/');
+        prefService.remove('token');
       },
       child: Container(
         padding: EdgeInsets.symmetric(
